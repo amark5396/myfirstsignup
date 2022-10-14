8af1a7d5d616427aed8d76b2e8c56dde693a9657
amark5396 <115699582+amark5396@users.noreply.github.com>
2022-10-14 02:23:56 -0700

    add initial website files

---
+++ app.js
@@ -0,0 +1,28 @@
+// console.log("hello world")
+// alert("hello world")
+// const mercy=document.getElementById("helloworld");
+// mercy.style.color="red"
+
+ 
+const greatGuy=4+5
+console.log(greatGuy)
+let a=5
+let b=3     
+console.log(a*b)
+
+const pen=(2+4)
+console.log(pen)
+let myself=11
+ console.log(myself++)
+ 
+ const numb=70*26
+ console.log(numb)
+ let j=70
+ let l=26
+ let k=15
+ console.log(numb-k)
+
+
+
+
+ 
